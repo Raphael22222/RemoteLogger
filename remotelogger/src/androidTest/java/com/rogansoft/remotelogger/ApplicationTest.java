b1fcdaede6cf1d3fa2da8d77afd6d6c70d624a68
Raphael22222 <123850859+Raphael22222@users.noreply.github.com>
2024-09-12 02:32:40 +0100

    Atualizar o ApplicationTest.java

--- remotelogger/src/androidTest/java/com/rogansoft/remotelogger/ApplicationTest.java
+++ remotelogger/src/androidTest/java/com/rogansoft/remotelogger/ApplicationTest.java
@@ -9,5 +9,3 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
-    }
-}
